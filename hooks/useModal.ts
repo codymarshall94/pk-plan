e7ef25e18bc6115f813d6228838427c841fbfe42
Cody Marshall <codymstuntsx@gmail.com>
2023-07-10 12:50:36 -0700

    Hook for creating modal state

---
+++ hooks/useModal.ts
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+const useModal = () => {
+  const [open, setOpen] = useState(false);
+
+  const toggle = () => {
+    setOpen(!open);
+  };
+
+  return {
+    open,
+    toggle,
+  };
+};
+
+export default useModal;
