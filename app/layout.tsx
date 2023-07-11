946b8c4c4e2c82c13dcf106ee1dbe7d469618154
Cody Marshall <codymstuntsx@gmail.com>
2023-07-10 19:25:45 -0700

    updated to wrap components in modal provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,29 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import ManageModalProvider from "@/contexts/ModalContext";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Sidebar from "@/components/Sidebar/Sidebar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'PKPLAN',
-  description: 'An application for your parkour needs',
-}
+  title: "PKPLAN",
+  description: "An application for your parkour needs",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body> 
+      <ManageModalProvider>
+        <body className="flex">
+          <Sidebar />
+          {children}
+        </body>
+      </ManageModalProvider>
     </html>
-  )
+  );
 }
